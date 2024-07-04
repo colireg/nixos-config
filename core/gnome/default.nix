53a6d4877daad9d32ce20b7ec39b17c5f1aeae2d
matty <mattyraud@gmail.com>
2024-07-04 21:35:30 +0330

    fix: Consistency

--- core/gnome/default.nix
+++ core/gnome/default.nix
@@ -1,12 +1,10 @@
 { pkgs, config, ... }:
 
 {
-  imports = [
-    ./dconf.nix
-    ./packages.nix
-  ];
+  imports = [ ./dconf.nix ./packages.nix ];
 
   services.xserver = {
+    enabe = true;
     desktopManager.gnome.enable = true;
     displayManager.gdm.enable = true;
   };
