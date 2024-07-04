0222ee5e329d0a81071acfc825210ae7d46061fe
matty <mattyraud@gmail.com>
2024-07-04 21:35:19 +0330

    feat: Added more tools

--- matty/packages.nix
+++ matty/packages.nix
@@ -14,6 +14,7 @@
     chromium
     tor-browser-bundle-bin
     obsidian
+    imhex
 
     # Tools
     hunspell # Spell check for LibreOffice
--- tartarus/packages.nix
+++ tartarus/packages.nix
@@ -14,6 +14,7 @@
     jq
     nnn
     jcal
+    lsof
 
     nixfmt
     statix
