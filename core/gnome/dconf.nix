4f41f2e2bdb8680b3788fa6ef8e05e0b00a5a556
matty <mattyraud@gmail.com>
2024-07-28 15:05:16 +0330

    fix(gnome): Don't show warning when exiting console

--- core/gnome/dconf.nix
+++ core/gnome/dconf.nix
@@ -85,7 +85,7 @@
 
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
         binding = "<Super>Return";
-        command = "kgx";
+        command = "kgx -e tmux -u attach -t default";
         name = "Open Console";
       };
 
