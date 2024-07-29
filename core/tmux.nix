ce04c6b687a6f63aed9ee0ddd343a76f5279500b
matty <mattyraud@gmail.com>
2024-07-29 09:09:18 +0330

    config(tmux): Add shortcut for creating new session

--- core/tmux.nix
+++ core/tmux.nix
@@ -10,6 +10,7 @@
       bind-key -T copy-mode-vi v send -X begin-selection
       bind-key -T copy-mode-vi V send -X select-line
       bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
+      bind-key C-n command-prompt -p "New session:" "new-session -s '%%'"
     '';
   };
   environment.systemPackages = [ pkgs.xclip ];
