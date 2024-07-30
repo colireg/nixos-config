5008bb3b15ce11eae58fc13c478e1e6cb3cfdf7a
matty <mattyraud@gmail.com>
2024-07-30 20:36:44 +0330

    Add nixos unstable

--- flake.nix
+++ flake.nix
@@ -3,39 +3,48 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }:
+  outputs = inputs@{ nixpkgs, nixpkgs-unstable, home-manager, ... }:
     let
       username = "matty";
       email = "mattyraud@gmail.com";
+      pkgs-unstable = import nixpkgs-unstable {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
     in {
       nixosConfigurations.tartarus = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = { inherit inputs username email; };
+        specialArgs = { inherit inputs username email pkgs-unstable; };
         modules = [
           ./tartarus
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs username email; };
+            home-manager.extraSpecialArgs = {
+              inherit inputs username email pkgs-unstable;
+            };
             home-manager.users.matty = import ./matty;
           }
         ];
       };
       nixosConfigurations.hades = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = { inherit inputs username email; };
+        specialArgs = { inherit inputs username email pkgs-unstable; };
         modules = [
           ./hades
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs username email; };
+            home-manager.extraSpecialArgs = {
+              inherit inputs username email pkgs-unstable;
+            };
             home-manager.users.matty = import ./matty;
           }
         ];
