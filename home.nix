d7825c03b0a4ce3523b4153234f7209941e2dcec
matty <mattyraud@gmail.com>
2024-12-17 17:27:54 +0330

    feat: Remove neovim from homemanager

--- home.nix
+++ home.nix
@@ -45,62 +45,6 @@ lib.mkMerge [
     services.mpris-proxy.enable = true;
   }
   {
-    # Neovim
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      extraLuaConfig = builtins.readFile ./nvim/init.lua;
-      extraPackages = with pkgs; [
-        rust-analyzer
-        lua-language-server
-        nixd
-        xclip
-        pyright
-        pylint
-        elixir-ls
-      ];
-
-      plugins = with pkgs.vimPlugins; [
-        # LSP
-        nvim-lspconfig
-        nvim-cmp
-        cmp-buffer
-        cmp-path
-        cmp_luasnip
-        cmp-nvim-lsp
-        cmp-nvim-lua
-        luasnip
-        friendly-snippets
-
-        {
-          plugin = telescope-nvim;
-          config = builtins.readFile ./nvim/telescope.lua;
-          type = "lua";
-        }
-        {
-          plugin = nvim-treesitter.withAllGrammars;
-          config = builtins.readFile ./nvim/treesitter.lua;
-          type = "lua";
-        }
-        {
-          plugin = lsp-zero-nvim;
-          config = builtins.readFile ./nvim/lsp-zero.lua;
-          type = "lua";
-        }
-        {
-          plugin = undotree;
-          config = builtins.readFile ./nvim/undotree.lua;
-          type = "lua";
-        }
-        {
-          plugin = vim-fugitive;
-          config = builtins.readFile ./nvim/fugitive.lua;
-          type = "lua";
-        }
-      ];
-    };
-  }
-  {
     # Misc
     home.username = "matty";
     home.homeDirectory = "/home/matty";
