b3a2e76ad03aefc99a512b2bbbae0b8ae4f99b2d
matty <mattyraud@gmail.com>
2024-12-17 16:34:48 +0330

    refactor: Organize hades configuration

--- hades/configuration.nix
+++ hades/configuration.nix
@@ -1,5 +1,9 @@
-{ pkgs, ... }:
-
+{
+  pkgs,
+  lib,
+  username,
+  ...
+}:
 {
   imports = [
     ./hardware-configuration.nix
@@ -13,81 +17,92 @@
     ../core/tor.nix
     ../core/nvim.nix
     ../core/zsh.nix
+    {
+      # Boot
+      boot.supportedFilesystems = [ "ntfs" ];
+      boot.loader.systemd-boot.enable = true;
+      boot.loader.efi.canTouchEfiVariables = true;
+    }
+    {
+      # ZFS
+      boot.supportedFilesystems = [ "zfs" ];
+      boot.zfs.allowHibernation = true;
+      boot.zfs.forceImportRoot = false;
+      networking.hostId = "a0739256";
+      services.zfs.autoScrub.enable = true;
+    }
+    {
+      # Network
+      networking.hostName = "hades";
+      networking.nftables.enable = true;
+      networking.wireless.iwd.enable = true;
+      networking.networkmanager.enable = true;
+      networking.networkmanager.wifi.backend = "iwd";
+      networking.firewall.allowPing = true;
+      users.users.${username}.extraGroups = [ "networkmanager" ];
+    }
+    {
+      # Packages
+      environment.systemPackages = with pkgs; [
+        wget
+        openssh
+        zip
+        unzip
+        parted
+        bat # cat
+        ripgrep
+        ncdu # disk usage
+        duf
+        jq
+        jcal
+        lsof
+        restic
+        socat
+        mitmproxy
+        tealdeer
+
+        elixir
+        elixir-ls
+        erlang
+        erlang-ls
+        nixfmt-rfc-style
+        nixd
+      ];
+      programs.htop.enable = true;
+      programs.gnupg.agent.enable = true;
+      security.pki.certificateFiles = [ /cert/mitmproxy-ca-cert.pem ];
+    }
+    {
+      # Users
+      users.users.${username} = {
+        isNormalUser = true;
+        extraGroups = [
+          "wheel"
+          "libvirtd"
+          "docker"
+          "tor"
+          "incus-admin"
+        ];
+      };
+      nix.settings.trusted-users = [ "matty" ];
+    }
+    {
+      # Nix
+      nixpkgs.config.allowUnfree = true;
+      nix.settings.experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      programs.nix-ld.enable = true;
+    }
+    {
+      # Misc
+      services.pcscd.enable = true;
+      console.earlySetup = true;
+      time.timeZone = "Asia/Tehran";
+      i18n.defaultLocale = "en_US.UTF-8";
+      zramSwap.enable = true;
+      system.stateVersion = "24.11";
+    }
   ];
-
-  boot.supportedFilesystems = [
-    "ntfs"
-    "zfs"
-  ];
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.zfs.allowHibernation = true;
-  boot.zfs.forceImportRoot = false;
-
-  networking.hostName = "hades";
-  networking.nftables.enable = true;
-  networking.wireless.iwd.enable = true;
-  networking.networkmanager.enable = true;
-  networking.networkmanager.wifi.backend = "iwd";
-  networking.firewall.allowPing = true;
-  networking.hostId = "a0739256";
-
-  environment.systemPackages = with pkgs; [
-    wget
-    openssh
-    zip
-    unzip
-    parted
-    bat # cat
-    ripgrep
-    ncdu # disk usage
-    duf
-    jq
-    jcal
-    lsof
-    restic
-    socat
-    mitmproxy
-    tealdeer
-
-    elixir
-    elixir-ls
-    erlang
-    erlang-ls
-    nixfmt-rfc-style
-    nixd
-  ];
-  programs.htop.enable = true;
-
-  users.users.matty = {
-    isNormalUser = true;
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-      "libvirtd"
-      "docker"
-      "tor"
-      "incus-admin"
-    ];
-  };
-  nix.settings.trusted-users = [ "matty" ];
-
-  services.pcscd.enable = true;
-  programs.gnupg.agent.enable = true;
-
-  security.pki.certificateFiles = [ /cert/mitmproxy-ca-cert.pem ];
-  programs.nix-ld.enable = true;
-  services.zfs.autoScrub.enable = true;
-
-  console.earlySetup = true;
-
-  nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-  time.timeZone = "Asia/Tehran";
-  i18n.defaultLocale = "en_US.UTF-8";
-  zramSwap.enable = true;
-  system.stateVersion = "24.11";
 }
