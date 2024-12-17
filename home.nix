984197546890f3a41ff24a8eb1720bf5cda2e9cf
matty <mattyraud@gmail.com>
2024-12-17 17:25:25 +0330

    refactor: Move homemanager related configs to home.nix

--- home.nix
+++ home.nix
@@ -1,12 +1,14 @@
-{ pkgs, pkgs-unstable, ... }:
-
 {
-  imports = [
-    ./nvim
-  ];
+  pkgs,
+  pkgs-unstable,
+  lib,
+  ...
+}:
 
-  home.packages =
-    (with pkgs; [
+lib.mkMerge [
+  {
+    # Packages
+    home.packages = with pkgs; [
       # Gui
       vlc
       libreoffice
@@ -25,21 +27,84 @@
       mpv
       papirus-icon-theme
       emacs-gtk
-    ])
-    ++ (with pkgs-unstable; [
+    ];
+    programs.vscode = {
+      enable = true;
+      package = pkgs.vscode-fhs;
+    };
+  }
+  {
+    # Unstable packages
+    home.packages = with pkgs-unstable; [
       zed-editor
       qbittorrent
-    ]);
+    ];
+  }
+  {
+    # Bluetooth
+    services.mpris-proxy.enable = true;
+  }
+  {
+    # Neovim
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      extraLuaConfig = builtins.readFile ./nvim/init.lua;
+      extraPackages = with pkgs; [
+        rust-analyzer
+        lua-language-server
+        nixd
+        xclip
+        pyright
+        pylint
+        elixir-ls
+      ];
 
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscode-fhs;
-  };
+      plugins = with pkgs.vimPlugins; [
+        # LSP
+        nvim-lspconfig
+        nvim-cmp
+        cmp-buffer
+        cmp-path
+        cmp_luasnip
+        cmp-nvim-lsp
+        cmp-nvim-lua
+        luasnip
+        friendly-snippets
 
-  # For bluetooth
-  services.mpris-proxy.enable = true;
-  home.username = "matty";
-  home.homeDirectory = "/home/matty";
-  home.stateVersion = "24.11";
-  programs.home-manager.enable = true;
-}
+        {
+          plugin = telescope-nvim;
+          config = builtins.readFile ./nvim/telescope.lua;
+          type = "lua";
+        }
+        {
+          plugin = nvim-treesitter.withAllGrammars;
+          config = builtins.readFile ./nvim/treesitter.lua;
+          type = "lua";
+        }
+        {
+          plugin = lsp-zero-nvim;
+          config = builtins.readFile ./nvim/lsp-zero.lua;
+          type = "lua";
+        }
+        {
+          plugin = undotree;
+          config = builtins.readFile ./nvim/undotree.lua;
+          type = "lua";
+        }
+        {
+          plugin = vim-fugitive;
+          config = builtins.readFile ./nvim/fugitive.lua;
+          type = "lua";
+        }
+      ];
+    };
+  }
+  {
+    # Misc
+    home.username = "matty";
+    home.homeDirectory = "/home/matty";
+    home.stateVersion = "24.11";
+    programs.home-manager.enable = true;
+  }
+]
