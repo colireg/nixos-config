8b45b301cb41909fd4d2013f38a239f4cd019a85
matty <mattyraud@gmail.com>
2024-12-18 09:27:41 +0330

    feat: Remove zsh config from nix

--- configuration.nix
+++ configuration.nix
@@ -50,31 +50,19 @@ lib.mkMerge [
     };
   }
   {
-    # Zsh
-    programs.zsh = {
-      enable = true;
-      syntaxHighlighting.enable = true;
-      enableCompletion = true;
-      autosuggestions.enable = true;
-      autosuggestions.strategy = [ "completion" ];
-      shellInit = ''
-        bindkey '^[[A' fzf-history-widget
-      '';
-    };
+    # Shell
+    programs.zsh.enable = true;
     environment.shells = [ pkgs.zsh ];
     users.defaultUserShell = pkgs.zsh;
 
     # Get completion for system packages for zsh
     environment.pathsToLink = [ "/share/zsh" ];
-    environment.sessionVariables = {
-      ZSH_TMUX_UNICODE = "true";
-      ZSH_TMUX_DEFAULT_SESSION_NAME = "default";
-    };
 
-    programs.fzf.fuzzyCompletion = true;
-    programs.fzf.keybindings = true;
+    environment.systemPackages = with pkgs; [
+      nix-index
+      fzf
+    ];
 
-    environment.systemPackages = with pkgs; [ nix-index ];
     programs.command-not-found.enable = false;
     programs.zsh.interactiveShellInit = ''
       source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
