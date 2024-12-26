fb95252a666a4c4dc8fe5ebde58a65afea06133a
matty <mattyraud@gmail.com>
2024-12-26 18:12:48 +0330

    Switch to Xfce

--- configuration.nix
+++ configuration.nix
@@ -36,29 +36,21 @@ lib.mkMerge [
   }
   {
     # Display
-    services.displayManager.defaultSession = "gnome";
     services.xserver = {
       enable = true;
-      desktopManager.gnome.enable = true;
-      displayManager.gdm.enable = true;
-      displayManager.gdm.wayland = true;
+      desktopManager = {
+        xterm.enable = false;
+        xfce.enable = true;
+      };
     };
-    environment.systemPackages = (
-      with pkgs;
-      [
-        foliate # EPUB reader
-        gnome-epub-thumbnailer # EPUB thumbnailer for Nautilous
-        gnome-tweaks
-        gucharmap # Character map
-        gnome-solanum # Pomodoro timer
-        newsflash # RSS reader
-      ]
-    );
-    environment.sessionVariables = {
-      NIXOS_OZONE_WL = "1"; # VSCode wayland
-      MOZ_ENABLE_WAYLAND = "1"; # Firefox wayland
-      ELECTRON_OZONE_PLATFORM_HINT = "auto";
-    };
+    services.displayManager.defaultSession = "xfce";
+    security.pam.services.gdm.enableGnomeKeyring = true;
+    services.gnome.gnome-keyring.enable = true;
+    services.libinput.touchpad.naturalScrolling = true;
+    environment.systemPackages = with pkgs; [
+      arc-theme
+      papirus-icon-theme
+    ];
   }
   {
     # Font
@@ -212,6 +204,8 @@ lib.mkMerge [
       vscode-fhs
       qbittorrent
       emacs-gtk
+      xarchiver
+      foliate
     ];
   }
   {
