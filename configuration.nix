c03c95cfb68116eb4280395bd702d54599a5a46c
matty <mattyraud@gmail.com>
2024-12-26 09:57:42 +0330

    Remove tmux config from nix

--- configuration.nix
+++ configuration.nix
@@ -8,22 +8,6 @@
 
 lib.mkMerge [
   {
-    # Tmux
-    programs.tmux = {
-      enable = true;
-      clock24 = true;
-      extraConfig = ''
-        set -g mouse on
-        set-window-option -g mode-keys vi
-        bind-key -T copy-mode-vi v send -X begin-selection
-        bind-key -T copy-mode-vi V send -X select-line
-        bind-key -T copy-mode-vi y send -X copy-pipe-and-cancel 'xclip -in -selection clipboard'
-        bind-key C-n command-prompt -p "New session:" "new-session -s '%%'"
-      '';
-    };
-    environment.systemPackages = [ pkgs.xclip ];
-  }
-  {
     # Tor
     services.tor = {
       enable = true;
@@ -183,7 +167,7 @@ lib.mkMerge [
       mitmproxy
       tealdeer
       fzf
-
+      xclip # for tmux
       elixir
       elixir-ls
       erlang
@@ -199,6 +183,7 @@ lib.mkMerge [
       enable = true;
       lfs.enable = true;
     };
+    programs.tmux.enable = true;
   }
   {
     # Users
